15b665fddb5f577dc5cc2f41359291edc10eb585
Daniel Eke <endanke@gmail.com>
2020-06-22 08:59:36 +0300

    Update code formatting

--- Observator/Observator.swift
+++ Observator/Observator.swift
@@ -7,17 +7,19 @@
 
 import Foundation
 
-fileprivate var observators: [String:AnyObject] = [:]
+// Need to be stored externally due to the generic type setup
+fileprivate var observators: [String: AnyObject] = [:]
 
 open class Observator<T> {
-    
+
     private static var identifier: String {
         return "\(String(describing: Self.self))\(String(describing: T.self))"
     }
-    
+
     public static var shared: Self {
-        if let singleton = observators[Self.identifier] {
-            return singleton as! Self
+        if let singleton = observators[Self.identifier],
+            let instance = singleton as? Self {
+            return instance
         } else {
             let instance = Self.init()
             observators[Self.identifier] = instance
@@ -35,44 +37,44 @@ open class Observator<T> {
             qos: .default, attributes: .concurrent
         )
     }
-    
-    private var _data: T?
+
+    private var internalData: T?
     public var data: T? {
         get {
-            var data: T? = nil
+            var data: T?
             queue.sync {
-                if let _data = self._data {
-                    data = self.readTransform(_data)
+                if let internalData = self.internalData {
+                    data = self.readTransform(internalData)
                 }
             }
             return data
         }
         set {
             queue.async(flags: .barrier) {
-                self._data = newValue
+                self.internalData = newValue
             }
             NotificationCenter.default.post(name: notificationName, object: nil)
         }
     }
-    
-    required public init(){}
-    
+
+    required public init() {}
+
     // Will be applied on the getter of the data when it's not nil
     open func readTransform(_ input: T) -> T {
         return input
     }
-    
+
     public func subscribe(_ observer: Any, selector: Selector) {
         NotificationCenter.default.addObserver(
             observer, selector: selector,
             name: notificationName, object: nil
         )
     }
-    
+
     public func unsubscribe(_ observer: Any) {
         NotificationCenter.default.removeObserver(
             observer, name: notificationName, object: nil
         )
     }
-    
+
 }
